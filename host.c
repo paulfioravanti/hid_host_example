5ca81687560002326ec6e5b839ca5c0777d0dd42
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-02-13 15:54:31 +1100

    Search devices with usage and usage_page

--- host.c
+++ host.c
@@ -11,18 +11,36 @@ enum {
 int main(int argc, char *argv[]) {
   hid_init();
 
-  hid_device *handle = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
-  if (!handle) {
-    printf("Unable to open device\n");
-    hid_exit();
-    return 1;
+  hid_device *handle = NULL;
+  struct hid_device_info *devices, *current_device;
+  devices = hid_enumerate(VENDOR_ID, PRODUCT_ID);
+  current_device = devices;
+
+  while (current_device) {
+    unsigned short int usage_page = current_device->usage_page;
+    unsigned short int usage = current_device->usage;
+
+    printf("Opening -- Usage (page): 0x%hX (0x%hX)...\n", usage, usage_page);
+    handle = hid_open_path(current_device->path);
+
+    if (!handle) {
+      printf("Unable to open device\n");
+      current_device = current_device->next;
+      continue;
+    }
+
+    printf("Success!\n");
+    break;
   }
 
-  wchar_t manufacturer[MAX_LENGTH];
-  hid_get_manufacturer_string(handle, manufacturer, MAX_LENGTH);
-  printf("Manufacturer string: %ls\n", manufacturer);
-
-  hid_close(handle);
+  if (handle) {
+    wchar_t manufacturer[MAX_LENGTH];
+    hid_get_manufacturer_string(handle, manufacturer, MAX_LENGTH);
+    printf("Manufacturer string: %ls\n", manufacturer);
+    hid_close(handle);
+  } else {
+    printf("Unable to open any devices for 0x%hX:0x%hX\n", VENDOR_ID, PRODUCT_ID);
+  }
 
   hid_exit();
 
